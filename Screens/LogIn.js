c0236a62b0880ee94b55206602e8668c089a7f16
Atobiloye Usama Adedayo <Osama@Atobiloyes-MacBook-Pro.local>
2020-06-02 18:13:12 +0100

    Intergrated Facebook login with firebase

--- Screens/LogIn.js
+++ Screens/LogIn.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase'
+import * as Facebook from 'expo-facebook'
+
+var provider = new firebase.auth.FacebookAuthProvider()
+
 
 class LogIn extends Component {
     constructor(props) {
@@ -7,6 +12,10 @@ class LogIn extends Component {
 
         this.handlePressIn = this.handlePressIn.bind(this)
         this.handlePressOut = this.handlePressOut.bind(this)
+    }
+
+    state = {
+        name: ''
     }
 
 
@@ -18,24 +27,103 @@ class LogIn extends Component {
         Animated.spring(this.animatedValue, {toValue: 1, friction: 2, tension: 50}).start()
     }
 
-    componentWillMount() {
-        this.animatedValue = new Animated.Value(1)
+    // componentDidMount() {
+    //     // this.animatedValue = new Animated.Value(1);
+    //     firebase.auth().onAuthStateChanged((user)=>{
+    //         if (user != null) {
+    //             console.log(user)
+    //         }
+    //     })
 
+    // }
+
+    
+
+    // async loginWithFacebook() {
+
+    //     const {type, token} = await Facebook.logInWithReadPermissionsAsync('566325950929908', {permissions: ['public_profile', 'email']})
+    //     if (type === 'success') {
+            
+               
+            
+    //         const credential = firebase.auth.FacebookAuthProvider.credential(token)
+    //         firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {console.log(error)})
+           
+    //     }
+        
+    // }
+
+    getUsername =()=> {
+        firebase.auth().onAuthStateChanged((user)=> {
+            if (user != null) {
+                console.log(user.displayName)
+                this.props.navigation.navigate('Home')
+                this.setState({name: user.displayName})
+            } else {
+                this.setState({name: ''})
+            }
+        })
     }
+
+    async logIn() {
+        try {
+          await Facebook.initializeAsync('566325950929908');
+          const {
+            type,
+            token,
+            expires,
+            permissions,
+            declinedPermissions,
+          } = await Facebook.logInWithReadPermissionsAsync({
+            permissions: ['public_profile'],
+          });
+          if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+            Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+            const credential = firebase.auth.FacebookAuthProvider.credential(token)
+            firebase.auth().signInWithCredential(credential).catch((error) => {console.log(error)})
+            
+          } else {
+            // type === 'cancel'
+          }
+        } catch ({ message }) {
+          alert(`Facebook Login Error: ${message}`);
+        }
+      }
+
+      signOut=()=> {
+          firebase.auth().signOut().then(function() {
+              console.log('Signed Out')
+          }).catch(function(error) {
+              console.log(error)
+          })
+      }
 
   render() {
       const animatedStyle = {
           transform: [{scale: this.animatedValue}]
       }
+
+     
     return (
       <View style={styles.container}>
-          <TouchableWithoutFeedback
+          {/* <TouchableWithoutFeedback
                 onPressIn={this.handlePressIn}
                 onPressOut={this.handlePressOut}>
           <Animated.View style={[styles.button, animatedStyle]}>
             <Text style={{color:'#fff'}}>Login</Text>
             </Animated.View>
-          </TouchableWithoutFeedback>
+          </TouchableWithoutFeedback> */}
+
+          <TouchableOpacity onPress={()=>{this.logIn(); this.getUsername()}}>
+          <Text style={{color:'#333'}}>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=>this.signOut()}>
+          <Text style={{color:'#333'}}>signOut</Text>
+          </TouchableOpacity>
+        <Text>{this.state.name}</Text>
         
       </View>
     );
